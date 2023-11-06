7d24a2fe57801e1e6243550b8f93354993637b6a
ChristianDellarosa <christiandellarosa98@gmail.com>
2023-11-06 18:28:25 -0300

    Implement new relic

---
+++ newrelic.js
@@ -0,0 +1,7 @@
+exports.config = {
+  app_name: ['library-service'],
+  license_key: '9eb0c232ce37b3ac19c9f2898ffd91c9FFFFNRAL',
+  logging: {
+    level: 'info',
+  },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TerminusModule, HttpModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
