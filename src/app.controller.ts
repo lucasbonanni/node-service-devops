2b83510484673c9fcce254774135b49c0e353147
ChristianDellarosa <christiandellarosa98@gmail.com>
2023-11-07 20:00:01 -0300

    Implement new relic (#11)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/:id")
+  @Get("/code/:id")
   getByCode(@Param('id') id: string): any {
     return this.appService.getByCode(+id);
   }
