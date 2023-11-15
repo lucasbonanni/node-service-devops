9974892a64c4bd8bc19742b545499714c4ce1a77
ChristianDellarosa <christiandellarosa98@gmail.com>
2023-11-14 21:17:24 -0300

    Add endpoint add code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,30 @@
-import { ForbiddenException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, ForbiddenException, ImATeapotException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CodeDto } from './code.dto';
 
 @Injectable()
 export class AppService {
 
   static readonly SUCCESSFULLY_MESSAGE = "Successfully"
 
-  getByCode(code: number): any {
-    if(code === 0) {
+  getByCode(codeValue: number): any {
+    if(codeValue === 0) {
       throw new ForbiddenException("El numero no puede ser 0")
     }
 
-    if(code %2 === 0) {
+    if(codeValue %2 === 0) {
       throw new InternalServerErrorException("Error, el numero es par")
     }
 
     return { status: AppService.SUCCESSFULLY_MESSAGE }
-}
+  }
+
+  addCode(code: CodeDto): any {
+    if(code.value === 0) {
+      throw new ConflictException("El codigo no puede ser 0")
+    }
+    if(code.value === 100) {
+      throw new ImATeapotException("Que se yo")
+    }
+    return { status: AppService.SUCCESSFULLY_MESSAGE }
+  }
 }
